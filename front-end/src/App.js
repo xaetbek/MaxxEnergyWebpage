45ce2069f46bc3cd5b5bb20ae6d30a86a0a1c398
Hayat A <xaetbek@gmail.com>
2025-08-11 11:52:39 -0700

    Created a reusable navbar component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <main style={{ padding: "20px" }}>
+        <h1>Welcome to My Website</h1>
+        <p>This is the homepage content.</p>
+      </main>
     </div>
   );
 }
